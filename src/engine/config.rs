baa5eba433833a63eb9bc14cfc6799c68749515a
Kyle Finlay <kyle.finlay01@gmail.com>
2016-06-10 16:24:38 +1000

    Config manager can query for config files

--- src/engine/config.rs
+++ src/engine/config.rs
@@ -14,8 +14,27 @@
 // limitations under the License.
 /*===============================================================================================*/
 
+extern crate glob;
 extern crate serde;
 extern crate serde_json;
+
+use self::glob::glob;
+
+use std::vec::Vec;
+
+/*===============================================================================================*/
+/*------CONFIG STRUCT----------------------------------------------------------------------------*/
+/*===============================================================================================*/
+
+#[derive (Clone)]
+pub struct Config {
+
+    // Public
+    /// The name of the config file.
+    pub name: String,
+    /// The path to the config file.
+    pub path: String
+}
 
 /*===============================================================================================*/
 /*------CONFIGMANAGER STRUCT---------------------------------------------------------------------*/
@@ -30,14 +49,49 @@ pub struct ConfigManager {
 
     // Public
     /// The path to the config directory.
-    pub config_dir: String
+    pub config_dir: String,
+
+    // Private
+    _config_list: Vec<Config>
 }
+
+/*===============================================================================================*/
+/*------CONFIGMANAGER PUBLIC METHODS-------------------------------------------------------------*/
+/*===============================================================================================*/
+
+impl ConfigManager {
+
+    /// Queries the config directory, and stores a list of configs.
+    pub fn query_config_dir (&mut self) {
+
+        // Clear the old config list
+        self._config_list.clear ();
+
+        info! ("Searching for config files...");
+
+        // Recurse through all items in the config directory
+        for path in glob (&format! ("{}/*{}", &self.config_dir, ".json")).unwrap ().filter_map (Result::ok) {
+
+            self._config_list.push (Config {name: path.file_stem ().unwrap ().to_str ().unwrap ().to_string (),
+                                            path: path.to_str ().unwrap ().to_string ()});
+
+            info! ("Found: {:?}", &path.file_name ().unwrap ());
+        }
+
+        // If list is empty, print empty
+        // Otherwise, print searching complete
+        if self._config_list.is_empty () {
+            info! ("No config files found.");
+        }
+
+        else {
+            info! ("Config searching complete.");
+        }
+    }
 
 /*===============================================================================================*/
 /*------CONFIGMANAGER PUBLIC STATIC METHODS------------------------------------------------------*/
 /*===============================================================================================*/
-
-impl ConfigManager {
 
     /// Returns a new instance of the config manager.
     ///
@@ -48,6 +102,7 @@ impl ConfigManager {
     /// A new config manager instance.
     pub fn new () -> ConfigManager {
 
-        ConfigManager {config_dir: "config".to_string ()}
+        ConfigManager {config_dir: "config".to_string (),
+                       _config_list: Vec::new ()}
     }
 }
